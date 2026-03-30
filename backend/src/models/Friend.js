f42be7df82ad31ed61235188fa87501e7c35fd13
NguyeVanLong <nvlong0712@gmail.com>
2026-03-30 15:34:34 +0700

    update part 2

---
+++ backend/src/models/Friend.js
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose';
+import { type } from 'os';
+const friendSchema = new mongoose.Schema(
+    {
+        userA: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        }, 
+        userB: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        }, 
+    },
+    {
+        timestamps: true,
+    }
+);
+
+friendSchema.pre('save', function() {
+    if(!this.userA || !this.userB) {
+        return;
+    }
+    
+    const a = this.userA.toString();
+    const b = this.userB.toString();
+    if(a > b){
+        [this.userA, this.userB] = [new mongoose.Types.ObjectId(b), new mongoose.Types.ObjectId(a)];
+    }
+});
+
+friendSchema.index({userA: 1, userB: 1}, {unique: true});
+
+const Friend = mongoose.model('Friend', friendSchema);
+
+export default Friend;
